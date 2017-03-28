4d9b368d92a59ec53f699d2cb94e76fc1e8bc801
Onno-Dirkzwager <onno@mentalsolutions.nl>
2017-03-28 16:04:48 +0200

    Update virginSoil-Full.ino
    update code

--- examples/5.VirginSoil-Full/virginSoil-Full.ino
+++ examples/5.VirginSoil-Full/virginSoil-Full.ino
@@ -1,1 +1,82 @@
 
+/* This is an initial sketch to be used as a "blueprint" to create apps which can be used with IOTappstory.com infrastructure
+  Your code can be filled wherever it is marked.
+
+
+  Copyright (c) [2016] [Andreas Spiess]
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+*/
+
+#define SKETCH "virginSoil "
+#define VERSION "V2.1.1"
+//#define LEDS_INVERSE          // LEDS on = GND
+
+// ================================================ PIN DEFINITIONS ======================================
+#ifdef ARDUINO_ESP8266_ESP01  // Generic ESP's 
+  #define MODEBUTTON 0
+  #define LEDgreen 13
+  //#define LEDred 12
+#else
+  #define MODEBUTTON D3
+  #define LEDgreen D7
+  //#define LEDred D6
+#endif
+  
+#include <IOTAppStory.h>
+#include <IAS_Xtra_Func.h>
+IOTAppStory IAS(SKETCH,VERSION,MODEBUTTON);
+
+
+
+// ================================================ SETUP ================================================
+void setup() {
+  IAS.serialdebug(true);                                                                                      // 1st parameter: true or false for serial debugging. Default: false
+  //IAS.serialdebug(true,115200);                                                                             // 1st parameter: true or false for serial debugging. Default: false | 2nd parameter: serial speed. Default: 115200
+
+  //IAS.preSetConfig("WireDirk","luchtschip");                                                                // preset Wifi
+  //IAS.preSetConfig("WireDirk","luchtschip",true);                                                           // preset Wifi & automaticUpdate
+  //IAS.preSetConfig("WireDirk","luchtschip","testboard");                                                    // preset Wifi & boardName
+  //IAS.preSetConfig("WireDirk","luchtschip","testboard",true);                                               // preset Wifi, boardName & automaticUpdate
+  //IAS.preSetConfig("WireDirk","luchtschip","testboard","iotappstory.com","/ota/esp8266-v1.php");            // preset Wifi, boardName, IOTappStory1 & IOTappStoryPHP1
+  //IAS.preSetConfig("WireDirk","luchtschip","testboard","iotappstory.com","/ota/esp8266-v1.php",true);       // preset Wifi, boardName, IOTappStory1, IOTappStoryPHP1 & automaticUpdate
+
+  //IAS.addField("ledpin", "Led Pin", &ledPin, 1);		<- this does not work yet
+  //IAS.addField("btnpin", "Button Pin", &btnPin, 1);	<- this does not work yet
+  //IAS.addField("rstpin", "Reset Pin", &rstPin, 1);    <- this does not work yet
+  /* TIP! delete the above lines when not used */
+
+  IAS.boot(p,true,LEDgreen);                                                                                    // 1st parameter: true or false to view BOOT STATISTICS | 2nd parameter: green feedback led integer | 3rd argument attach interrupt for the mode selection button
+
+
+  //-------- Your Setup starts from here ---------------
+  
+}
+
+
+// ================================================ LOOP =================================================
+void loop() {
+  yield();
+  IAS.routine(org_buttonEntry,org_buttonTime,org_buttonChanged);                                              // this routine handles the reaction of the Flash button. If short press: update of skethc, long press: Configuration
+
+
+  //-------- Your Sketch starts from here ---------------
+
+}
