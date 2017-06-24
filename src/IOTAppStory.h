4197a3afd321abdcd17a68c92fba50ed4655ac68
Onno-Dirkzwager <onno@mentalsolutions.nl>
2017-06-24 02:36:56 +0200

    changed MAX_WIFI_RETRIES
    from 50 to 15
    It took to long to run the "loader", maby 15 is too short....

--- src/IOTAppStory.h
+++ src/IOTAppStory.h
@@ -8,7 +8,7 @@
     #define MAXNUMEXTRAFIELDS 12
     #define MAGICEEP "%"
     #define UDP_PORT 514
-    #define MAX_WIFI_RETRIES 50
+    #define MAX_WIFI_RETRIES 15
     #define RTCMEMBEGIN 68
     #define MAGICBYTE 85
     #define STRUCT_CHAR_ARRAY_SIZE 50  // length of config variables
