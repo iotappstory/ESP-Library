b09ae6db3fe36af5a6af1abbf6e9067898b7acf2
Andreas Spiess <andreas.spiess@arumba.com>
2017-09-01 21:01:23 +0200

    Rename and enhance Loader

--- examples/IASLoader/IASLoader.ino
+++ examples/IASLoader/IASLoader.ino
@@ -33,13 +33,16 @@
 #include <IOTAppStory.h>
 IOTAppStory IAS(APPNAME, VERSION, COMPDATE, MODEBUTTON);
 
+unsigned long printEntry;
+String boardName;
 
 
 // ================================================ SETUP ================================================
 void setup() {
   IAS.serialdebug(true);                  // 1st parameter: true or false for serial debugging. Default: false
   //IAS.serialdebug(true,115200);         // 1st parameter: true or false for serial debugging. Default: false | 2nd parameter: serial speed. Default: 115200
-  IAS.preSetConfig("INITLoader", false);	// preset Boardname, automatic upload false
+  boardName = APPNAME"_" + WiFi.macAddress();
+  IAS.preSetConfig(boardName, false);	// preset Boardname, automatic upload false
 
   IAS.begin(true, true);									// 1st parameter: true or false to view BOOT STATISTICS | 2nd parameter: true or false to erase eeprom on first boot of the app
   IAS.callHome(true);
@@ -51,8 +54,10 @@ void setup() {
 
 // ================================================ LOOP =================================================
 void loop() {
-  // if the scetch reaches this spot, no project was defined. Otherwise, it would load the defined sketch already before...
-  Serial.println( "\n\n                               ----------------   N O   A P P   L O A D E D   ----------------");
-  Serial.println( "\n----------------  P L E A S E  C R E A T E   P R O J E C T   O N   I O T A P P S T O R Y . C O M   ----------------\n");
-  delay(5000);
+  if (millis() - printEntry > 5000) {
+    // if the scetch reaches this spot, no project was defined. Otherwise, it would load the defined sketch already before...
+    Serial.println( "\n\n                               ----------------   N O   A P P   L O A D E D   ----------------");
+    Serial.println( "\n----------------  P L E A S E  C R E A T E   P R O J E C T   O N   I O T A P P S T O R Y . C O M   ----------------\n");
+    printEntry = millis();
+  }
 }
