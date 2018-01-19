ce20bae8123629e37ef0530e610c65b8a9af6e58
Onno-Dirkzwager <onno@mentalsolutions.nl>
2018-01-19 21:31:40 +0100

    Set DHTTYPE to most used

--- examples/Wemos-Shield-DHT/Wemos-Shield-DHT.ino
+++ examples/Wemos-Shield-DHT/Wemos-Shield-DHT.ino
@@ -26,9 +26,9 @@
 
 
 // Uncomment whatever type you're using!
-//#define DHTTYPE DHT11       // DHT 11
+#define DHTTYPE DHT11       // DHT 11
 //#define DHTTYPE DHT21     // DHT 21 (AM2301)
-#define DHTTYPE DHT22     // DHT 22  (AM2302)
+//#define DHTTYPE DHT22     // DHT 22  (AM2302)
 
 
 // Connect pin 1 (on the left) of the sensor to +5V
