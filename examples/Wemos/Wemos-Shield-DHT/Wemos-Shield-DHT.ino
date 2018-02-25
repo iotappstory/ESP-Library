a3bfc075ac96dd3df926fe16e0f118c018e7f7a8
Onno-Dirkzwager <onno@mentalsolutions.nl>
2018-02-25 21:10:53 +0100

    Update Wemos-Shield-DHT.ino

--- examples/Wemos/Wemos-Shield-DHT/Wemos-Shield-DHT.ino
+++ examples/Wemos/Wemos-Shield-DHT/Wemos-Shield-DHT.ino
@@ -51,9 +51,9 @@
 
 
 // Uncomment whatever type you're using!
-//#define DHTTYPE DHT11                                     // DHT 11
+#define DHTTYPE DHT11                                     // DHT 11
 //#define DHTTYPE DHT21                                   // DHT 21 (AM2301)
-#define DHTTYPE DHT22                                   // DHT 22  (AM2302)
+//#define DHTTYPE DHT22                                   // DHT 22  (AM2302)
 
 
 WiFiClient espClient;
