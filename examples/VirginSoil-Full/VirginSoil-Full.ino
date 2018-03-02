d64c4955a17e428c6d5f0889fabce2b6769cce00
Onno-Dirkzwager <onno@mentalsolutions.nl>
2018-03-02 18:25:33 +0100

    Added examples of all the "special fields"

--- examples/VirginSoil-Full/VirginSoil-Full.ino
+++ examples/VirginSoil-Full/VirginSoil-Full.ino
@@ -44,34 +44,40 @@ unsigned long printEntry;
 // Use functions like atoi() and atof() to transform the char array to integers or floats
 // Use IAS.dPinConv() to convert Dpin numbers to integers (D6 > 14)
 
-char* lbl1        = "Light Show";
-char* lbl2        = "Living Room";
+char* lbl         = "Light Show";
 char* exampleURL  = "http://someapi.com/getdata.php?userid=1234&key=7890abc";
-char* timeZone    = "0.0";
+char* nrOf        = "6";
+
+char* doSomething = "1";
+char* chosen      = "0";
+
+char* updInt      = "60";
 char* ledPin      = "2";
+char* timeZone    = "0.0";
 
 
 
 // ================================================ SETUP ================================================
 void setup() {
   /* TIP! delete lines below when not used */
-  IAS.preSetDeviceName("virginsoil");                 // preset Boardname this is also your MDNS responder: http://virginsoil.local
-  //IAS.preSetAutoUpdate(false);                      // automaticUpdate (true, false)
-  //IAS.preSetAutoConfig(false);                      // automaticConfig (true, false)
-  //IAS.preSetWifi("ssid","password");                // preset Wifi
+  IAS.preSetDeviceName("virginsoil");                       // preset Boardname this is also your MDNS responder: http://virginsoil.local
+  //IAS.preSetAutoUpdate(false);                            // automaticUpdate (true, false)
+  //IAS.preSetAutoConfig(false);                            // automaticConfig (true, false)
+  //IAS.preSetWifi("ssid","password");                      // preset Wifi
   /* TIP! Delete Wifi cred. when you publish your App. */
-
-	
-  IAS.setCallHome(true);											        // Set to true to enable calling home frequently (disabled by default)
-  IAS.setCallHomeInterval(60);										    // Call home interval in seconds, use 60s only for development. Please change it to at least 2 hours in production
 	
 
-  IAS.addField(lbl1, "Label 1", 16);                  // These fields are added to the config wifimanager and saved to eeprom. Updated values are returned to the original variable.
-  IAS.addField(lbl2, "Label 2", 16);                  // reference to org variable | field label value | max char return
-  IAS.addField(exampleURL, "Example url", 80, 'T');
+  IAS.addField(lbl, "textLine", 16);                        // These fields are added to the "App Settings" page in config mode and saved to eeprom. Updated values are returned to the original variable.
+  IAS.addField(exampleURL, "Textarea", 80, 'T');            // reference to org variable | field label value | max char return | Optional "special field" char
+  IAS.addField(nrOf, "Number", 8, 'N');                     // Find out more about the optional "special fields" at https://iotappstory.com/wiki
+  
+  IAS.addField(doSomething, "Checkbox:Check me", 1, 'C');
+  IAS.addField(chosen, "Selectbox:Red,Green,Blue", 1, 'S');
+
+  IAS.addField(updInt, "Interval", 8, 'I');
+  IAS.addField(ledPin, "ledPin", 2, 'P');
   IAS.addField(timeZone, "Timezone", 4, 'Z');
-  IAS.addField(ledPin, "ledPin", 2, 'N');
-  /* TIP! delete the lines above when not used */
+  
 
 
   // You can configure callback functions that can give feedback to the app user about the current state of the application.
@@ -125,10 +131,13 @@ void setup() {
   });
   */
 
-	
+	/* TIP! delete the lines above when not used */
+ 
   //IAS.begin();
-	//IAS.begin(true);                                  // 1st parameter: true or false to view BOOT STATISTICS
-  IAS.begin(true,'L');                                // 2nd parameter: Wat to do with EEPROM on First boot of the app? 'F' Fully erase | 'P' Partial erase(default) | 'L' Leave intact
+  IAS.begin('P');                                     // Optional parameter: What to do with EEPROM on First boot of the app? 'F' Fully erase | 'P' Partial erase(default) | 'L' Leave intact
+
+  IAS.setCallHome(true);                              // Set to true to enable calling home frequently (disabled by default)
+  IAS.setCallHomeInterval(60);                        // Call home interval in seconds, use 60s only for development. Please change it to at least 2 hours in production
 
 
   //-------- Your Setup starts from here ---------------
@@ -148,21 +157,25 @@ void loop() {
 
     Serial.println(F(" LABEL\t\t| VAR\t\t| VALUE"));
 
-    Serial.print(F(" Label 1\t| lbl1\t\t| "));
-    Serial.println(lbl1);
-
-    Serial.print(F(" Label 2\t| lbl2\t\t| "));
-    Serial.println(lbl2);
-
-    Serial.print(F(" Example url\t| exampleURL\t| "));
+    Serial.print(F(" textLine\t| lbl\t\t| "));
+    Serial.println(lbl);
+    Serial.print(F(" Textarea\t| exampleURL\t| "));
     Serial.println(exampleURL);
+    Serial.print(F(" Number\t\t| nrOf\t\t| "));
+    Serial.println(nrOf);
 
+    Serial.print(F(" Checkbox\t| doSomething\t| "));
+    Serial.println(doSomething);
+    Serial.print(F(" Selectbox\t| chosenColor\t| "));
+    Serial.println(chosen);
+
+    Serial.print(F(" Interval\t| updInt\t| "));
+    Serial.println(updInt);
+    Serial.print(F(" Led pin\t| ledPin\t| "));
+    Serial.println(atoi(ledPin));  
     Serial.print(F(" Timezone\t| timeZone\t| "));
     Serial.println(atof(timeZone));
 
-    Serial.print(F(" Led pin\t| ledPin\t| "));
-    Serial.println(atoi(ledPin));
-    
     Serial.println(F("*-------------------------------------------------------------------------*"));
     printEntry = millis();
   }
