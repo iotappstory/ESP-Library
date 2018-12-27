842b9166697ce3f1de6cfa7cbcb26aaed063c8c2
Onno-Dirkzwager <onno@mentalsolutions.nl>
2018-12-27 18:00:47 +0100

    updated next pins & baudrate

--- src/config.h
+++ src/config.h
@@ -32,12 +32,12 @@
 		#define NEXT_RES				5	// Nextion reset pin | Default 5 / D1 | Use this pin to control a transistor or relay to "hard" reset your display(power) after updates
 		#define NEXT_RX					14	// Nextion RX pin | Default 14 / D5
 		#define NEXT_TX					12	// Nextion TX pin | Default 12 / D6
-		#define NEXT_BAUD				57600	// Nextion baudrate | 115200 / 57600 <- seems more stable
+		#define NEXT_BAUD				115200	// Nextion baudrate | 115200 / 57600
 	#elif defined ESP32
 		#define NEXT_RES				5	// Nextion reset pin | Default 5 / D1 | Use this pin to control a transistor or relay to "hard" reset your display(power) after updates
-		#define NEXT_RX					27	// Nextion RX pin | Default 14 / D5
-		#define NEXT_TX					14	// Nextion TX pin | Default 12 / D6
-		#define NEXT_BAUD				57600	// Nextion baudrate | 115200 / 57600 <- seems more stable
+		#define NEXT_RX					16	// Nextion RX pin | Default 14 / D5
+		#define NEXT_TX					17	// Nextion TX pin | Default 12 / D6
+		#define NEXT_BAUD				115200	// Nextion baudrate | 115200 / 57600
 	#endif
  
 	// EERPOM & max nr of addable fields
