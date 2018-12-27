7447e42e9f0820cbe0af772fe988b41d13ec9eed
Onno-Dirkzwager <onno@mentalsolutions.nl>
2018-12-27 17:55:26 +0100

    updated includes, small cleanup & changed update file

--- src/espressif/esp32/otaUpdate.h
+++ src/espressif/esp32/otaUpdate.h
@@ -6,22 +6,20 @@
 		#include <WiFiClientSecure.h>
 		#include <StreamString.h>
 		#include <Update.h>
-		
-		#include <FS.h>														// esp32 core SPIFFS library
-		#include <SPIFFS.h>	
-		#include <HardwareSerial.h>
-		#include <ESPNexUpload.h>
+											 
+		#if NEXT_OTA == true
+			#include <ESPNexUpload.h>
+			#define U_NEXTION 300
+		#endif
 		
 		#define U_FLASH   0
 		#define U_SPIFFS  100
 		#define U_AUTH    200
-		#define U_NEXTION 300
 		
+
 		
 		struct strConfig;
-		///extern "C" uint32_t _SPIFFS_start;
-		///extern "C" uint32_t _SPIFFS_end;
-		///extern const char ROOT_CA[];
+		
 		
 		class otaUpdate{
 			
@@ -39,7 +37,7 @@
 				int _command;
 				uint32_t _totalSize 			= 0;										// total size of received update
 				const char* _updateHost		= "iotappstory.com";    // ota update host
-				const char* _updateFile		= "/ota/esp32-v1.php"; 	// file at host that handles 8266 updates
+				const char* _updateFile		= "/ota/esp32-v2.php"; 	// file at host that handles 8266 updates
 			
 				bool installESP();
 				bool installNEXTION();
