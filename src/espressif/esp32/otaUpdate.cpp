a0361908c59e7ec7874a21360d6e737367ba80d1
Onno-Dirkzwager <onno@mentalsolutions.nl>
2019-01-11 23:23:25 +0100

    Update otaUpdate.cpp

--- src/espressif/esp32/otaUpdate.cpp
+++ src/espressif/esp32/otaUpdate.cpp
@@ -255,7 +255,7 @@
 		});
 
 		// if nextion update failed return false & error
-		if(!nextion.prepairUpload(_totalSize)){
+		if(!nextion.prepareUpload(_totalSize)){
 			error = "Error: " + nextion.statusMessage;
 	 
 			return false;
