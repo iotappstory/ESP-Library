1f671d4812d285ebc8243cc1b4be4b02b36a04ec
Onno-Dirkzwager <onno@mentalsolutions.nl>
2019-05-05 22:03:11 +0200

    Update config.h
    Made HTTPS_8266_TYPE FNGPRINT default
    - for backwards compatibility with the previous 2.0.X library
    - Stability. Depending on use case you might run out of heap to early...

--- src/config.h
+++ src/config.h
@@ -18,11 +18,7 @@
 	#define CFG_PASS					"admin"		// initial authentication password. You edit & change this in config mode. | max 16 char
 	#define CFG_PAGE_INFO				true		// include the info page in Config mode
 	#define CFG_PAGE_IAS 				true  		// include the IAS page in Config mode
-	#if defined  ESP8266
-		#define CFG_ANNOUNCE			false		// Announce to IAS on which ip this device is. (unstable with ESP8266 BearSSL during Config mode)
-	#elif defined ESP32
-		#define CFG_ANNOUNCE			true
-	#endif
+	#define CFG_ANNOUNCE				true		// Announce to IAS on which ip this device is during Config mode. (unstable with HTTPS_8266_TYPE CERTIFICATE during Config due to not enough heap)
 	
 
 	// Wifi defines
@@ -45,9 +41,9 @@
 	
 	// HTTPS defines
 	#define HTTPS         				true				// Use HTTPS for OTA updates
-	#define HTTPS_8266_TYPE				CERTIFICATE			// FNGPRINT / CERTIFICATE | ESP32 only accepts certificates
+	#define HTTPS_8266_TYPE				FNGPRINT			// FNGPRINT / CERTIFICATE | ESP32 only accepts certificates | SET to FNGPRINT for backwards compatibility with 2.0.X
 	#define HTTPS_CERT_STORAGE			ST_SPIFFS			// ST_SPIFFS / ST_PROGMEM
-	#define HTTPS_FNGPRINT				"34:6D:0A:26:F0:40:3A:0A:1B:F1:CA:8E:C8:0C:F5:14:21:83:7C:B1" // Initial fingerprint. You can edit & change later in config mode.
+	#define HTTPS_FNGPRINT				"34 6d 0a 26 f0 40 3a 0a 1b f1 ca 8e c8 0c f5 14 21 83 7c b1" // Initial fingerprint. You can edit & change this later in config mode.
 	
 	
 	// OTA defines
