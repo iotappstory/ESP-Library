3094847cd7f55b6c0f636b273057a4cbe752ec54
Onno-Dirkzwager <onno@mentalsolutions.nl>
2019-06-11 00:45:17 +0200

    Copyright, Updated comment & New example: addDeviceToProject  (#125)
    * Added copyright & updated comment
    
    * Added addDeviceToProject example
      This sketch:
      - Erases EEPROM
      - Sets up initial hardcode WiFi connection
      - Contacts IOTAppStory.com over https
      - Registers your device with IOTAppStory.com
      - Adds the device to a project(product)
      - Saves the device activation code
      - Saves generated device name (eg. XXXX-22, XXXX-23 ...)
      - Saves the Fingerprint or Certificate (depends on config.h settings)
      - Saves the WiFi credentials (optional)
      - Calls home to get the latest firmware

--- examples/libraryUpgrader/upgradeHelper.ino
+++ examples/libraryUpgrader/upgradeHelper.ino
@@ -1,3 +1,25 @@
+/*
+  Copyright (c) [2019] [Onno Dirkzwager]
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
 char tempValue[NROFADDEDFIELDS][LARGESTLENGTH];
 
 
@@ -34,7 +56,7 @@ void runUpgrade(){
     
     /**
       If you need to "backup" old EEPROM values that are not within the config & added field structs. This is the place to put your code!
-      As we have the old config & added fields in memory and will be writing them in the lines below.
+      As we have the old config & added fields in memory and will after erasing all EEPROM write them in the lines below.
     */
 
     
