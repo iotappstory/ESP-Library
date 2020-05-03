5d528b704a5b7f1b3a66e549bbc94503295e8d84
Onno-Dirkzwager <onno@mentalsolutions.nl>
2020-05-04 00:14:05 +0200

    Update UpdateNextionClass.cpp
    Wrapped the code into an precompiler if statement to prevent the compiler from checking the code when OTA_UPD_CHECK_NEXTION is false and the Nextion library is not included:
    
    #include "../config.h"
    #if OTA_UPD_CHECK_NEXTION == true
    ... code ...
    #endif

--- src/espressif/UpdateNextionClass.cpp
+++ src/espressif/UpdateNextionClass.cpp
@@ -1,89 +1,92 @@
-/*                          =======================
-============================   C/C++ SOURCE FILE   ============================
-                            =======================                       *//**
-  UpdateNextion.cpp
+#include "../config.h"
+#if OTA_UPD_CHECK_NEXTION == true
+	/*                          =======================
+	============================   C/C++ SOURCE FILE   ============================
+								=======================                       *//**
+	  UpdateNextion.cpp
 
-  Created by Onno Dirkzwager on 10.02.2019.
-  Copyright (c) 2019 IOTAppStory. All rights reserved.
+	  Created by Onno Dirkzwager on 10.02.2019.
+	  Copyright (c) 2019 IOTAppStory. All rights reserved.
 
-*///===========================================================================
+	*///===========================================================================
 
-/*---------------------------------------------------------------------------*/
-/*                                INCLUDES                                   */
-/*---------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------*/
+	/*                                INCLUDES                                   */
+	/*---------------------------------------------------------------------------*/
 
-#include <Arduino.h>
-#include "UpdateNextionClass.h"
+	#include <Arduino.h>
+	#include "UpdateNextionClass.h"
 
-/*---------------------------------------------------------------------------*/
-/*                        DEFINITIONS AND MACROS                             */
-/*---------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------*/
+	/*                        DEFINITIONS AND MACROS                             */
+	/*---------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------------*/
-/*                        TYPEDEFS AND STRUCTURES                            */
-/*---------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------*/
+	/*                        TYPEDEFS AND STRUCTURES                            */
+	/*---------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------------*/
-/*                                PROTOTYPES                                 */
-/*---------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------*/
+	/*                                PROTOTYPES                                 */
+	/*---------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------------*/
-/*                            LOCAL VARIABLES                                */
-/*---------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------*/
+	/*                            LOCAL VARIABLES                                */
+	/*---------------------------------------------------------------------------*/
 
-UpdateNextionClass UpdateNextion;
+	UpdateNextionClass UpdateNextion;
 
-/*---------------------------------------------------------------------------*/
-/*                        FUNCTION IMPLEMENTATION                            */
-/*---------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------*/
+	/*                        FUNCTION IMPLEMENTATION                            */
+	/*---------------------------------------------------------------------------*/
 
-/*-----------------------------------------------------------------------------
-                        UpdateNextionClass prepareUpdate
+	/*-----------------------------------------------------------------------------
+							UpdateNextionClass prepareUpdate
 
-*///---------------------------------------------------------------------------
-bool UpdateNextionClass::prepareUpdate(uint32_t upd_size, String &upd_md5, uint16_t command) {
+	*///---------------------------------------------------------------------------
+	bool UpdateNextionClass::prepareUpdate(uint32_t upd_size, String &upd_md5, uint16_t command) {
 
-    // prepare upload: setup serial connection, send update command and send the expected update size
-    if(!this->nextion.prepareUpload(upd_size)) {
-        (*this->_statusMessage) = this->nextion.statusMessage;
-        return false;
-    }
+		// prepare upload: setup serial connection, send update command and send the expected update size
+		if(!this->nextion.prepareUpload(upd_size)) {
+			(*this->_statusMessage) = this->nextion.statusMessage;
+			return false;
+		}
 
-    return true;
-}
+		return true;
+	}
 
-/*-----------------------------------------------------------------------------
-                        UpdateNextionClass update
+	/*-----------------------------------------------------------------------------
+							UpdateNextionClass update
 
-*///---------------------------------------------------------------------------
-bool UpdateNextionClass::update(uint8_t *file_buf, size_t buf_size) {
+	*///---------------------------------------------------------------------------
+	bool UpdateNextionClass::update(uint8_t *file_buf, size_t buf_size) {
 
-    // Write the buffered bytes to the nextion display. If this fails, return false.
-    if(!this->nextion.upload(file_buf, buf_size)) {
-        (*this->_statusMessage) = this->nextion.statusMessage;
-        return false;
-    }
+		// Write the buffered bytes to the nextion display. If this fails, return false.
+		if(!this->nextion.upload(file_buf, buf_size)) {
+			(*this->_statusMessage) = this->nextion.statusMessage;
+			return false;
+		}
 
-    return true;
-}
+		return true;
+	}
 
-/*-----------------------------------------------------------------------------
-                        UpdateNextionClass end
+	/*-----------------------------------------------------------------------------
+							UpdateNextionClass end
 
-*///---------------------------------------------------------------------------
-bool UpdateNextionClass::end() {
-    // end: wait(delay) for the nextion to finish the update process, send nextion reset command and end the serial connection to the nextion
-    this->nextion.end();
-}
+	*///---------------------------------------------------------------------------
+	bool UpdateNextionClass::end() {
+		// end: wait(delay) for the nextion to finish the update process, send nextion reset command and end the serial connection to the nextion
+		this->nextion.end();
+	}
 
-/*-----------------------------------------------------------------------------
-                        UpdateNextionClass sm
+	/*-----------------------------------------------------------------------------
+							UpdateNextionClass sm
 
-*///---------------------------------------------------------------------------
-void UpdateNextionClass::sm(String *statusMessage) {
-    this->_statusMessage = statusMessage;
-}
+	*///---------------------------------------------------------------------------
+	void UpdateNextionClass::sm(String *statusMessage) {
+		this->_statusMessage = statusMessage;
+	}
 
-/*---------------------------------------------------------------------------*/
-/*                                    EOF                                    */
-/*---------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------*/
+	/*                                    EOF                                    */
+	/*---------------------------------------------------------------------------*/
+#endif
