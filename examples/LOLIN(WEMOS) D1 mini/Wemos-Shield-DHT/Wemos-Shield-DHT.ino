ea6fe37975a005a3e9a76bd187868761173283f6
Christiaan <CwlBroeders@gmail.com>
2020-07-25 00:31:52 +0200

    wemos lolin oled shield v2.1.0 fixes (#162)
    * update for lilin oled v2.1.0 compatability
    * fixed coding stylefixed typo

--- examples/LOLIN(WEMOS) D1 mini/Wemos-Shield-DHT/Wemos-Shield-DHT.ino
+++ examples/LOLIN(WEMOS) D1 mini/Wemos-Shield-DHT/Wemos-Shield-DHT.ino
@@ -1,8 +1,8 @@
 /*
   This is an initial sketch to be used as a "blueprint" to create apps which can be used with IOTappstory.com infrastructure
-  Your code can be added wherever it is marked. 
+  Your code can be added wherever it is marked.
 
-  You will need the button & OLED shields!
+  You will need either the button & OLED shields! or the LOLIN oled v2.1.0 shield!
 
   Copyright (c) [2018] [Andreas Spiess]
 
@@ -29,21 +29,23 @@
 #define MODEBUTTON D3
 
 
-#include <SSD1306.h>              // OLED library by Daniel Eichhorn
-#include <IOTAppStory.h>          // IotAppStory.com library
-#include <DHT.h>                  // Adafruit DHT Arduino library
-#include <PubSubClient.h>         // Arduino Client for MQTT by knolleary/pubsubclient
+#include <SSD1306.h>                                    // OLED library by Daniel Eichhorn
+#include <IOTAppStory.h>                                // IotAppStory.com library
+#include <DHT.h>                                        // Adafruit DHT Arduino library
+#include <PubSubClient.h>                               // Arduino Client for MQTT by knolleary/pubsubclient
+#include <LOLIN_I2C_BUTTON.h>                           // Wemos button library for wemos lolin oled shield v2.1.0 https://github.com/wemos/LOLIN_OLED_I2C_Button_Library
+I2C_BUTTON button(0x31);                                // I2C address 0x31
 
 
-#define PIN_RESET 255             //
-#define DC_JUMPER 0               // I2C Addres: 0 - 0x3C, 1 - 0x3D
-#define DHTPIN D4                 // what pin we're connected to
+#define PIN_RESET 255                                   //
+#define DC_JUMPER 0                                     // I2C Addres: 0 - 0x3C, 1 - 0x3D
+#define DHTPIN D4                                       // what pin we're connected to
 
 
 // Uncomment whatever type you are using!
-#define DHTTYPE DHT11                                     // DHT 11
-//#define DHTTYPE DHT21                                   // DHT 21 (AM2301)
-//#define DHTTYPE DHT22                                   // DHT 22  (AM2302)
+#define DHTTYPE DHT11                                   // DHT 11
+//#define DHTTYPE DHT21                                 // DHT 21 (AM2301)
+//#define DHTTYPE DHT22                                 // DHT 22  (AM2302)
 
 
 WiFiClient espClient;
@@ -70,45 +72,45 @@ char msg[50]            = "field1=22.5&field2=65.7&status=MQTTPUBLISH";
 // Use functions like atoi() and atof() to transform the char array to integers or floats
 // Use IAS.dPinConv() to convert Dpin numbers to integers (D6 > 14)
 
-char* updInt      = "60";                                 // every x sec
-char* scale       = "0";                                  // 0 = Celsius, 1 = Fahrenheit
-char* apikey      = "";                                   // ThingSpeak Write API Key
+char* updInt      = "60";                               // every x sec
+char* scale       = "0";                                // 0 = Celsius, 1 = Fahrenheit
+char* apikey      = "";                                 // ThingSpeak Write API Key
 
 
 
 // ================================================ SETUP ================================================
 void setup() {
-  display.init();                                                 // setup OLED and show "Wait"
-  display.flipScreenVertically();
-  display.clear();
-  display.setFont(ArialMT_Plain_16);
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.drawString(48, 35, F("Wait"));
-  display.display();
+  if(IAS.boardMode == 'N'){                             // setup OLED and show "Loading" Only in normal mode! Preserve heap for config mode.
+    display.init();
+    display.flipScreenVertically();
+    bootScreen(46, 14, F("Loading"),F("/wbmp/logo.WBMP"));
+    display.display();
+  }
 
 
   // creat a unique deviceName for classroom situations (deviceName-123)
   chipId      = String(ESP_GETCHIPID);
   chipId      = "-"+chipId.substring(chipId.length()-3);
   deviceName += chipId;
-  
+
   IAS.preSetDeviceName(deviceName);                               // preset Boardname this is also your MDNS responder: http://deviceName-123.local
 
 
   IAS.addField(updInt, "Update every", 8, 'I');                   // These fields are added to the config wifimanager and saved to eeprom. Updated values are returned to the original variable.
-  IAS.addField(scale, "Temp. scale:Celsius,Fahrenheit", 1, 'S');  // reference to org variable | field label value | max char return | Optional "special field" char 
+  IAS.addField(scale, "Temp. scale:Celsius,Fahrenheit", 1, 'S');  // reference to org variable | field label value | max char return | Optional "special field" char
   IAS.addField(apikey, "TS Write API Key", 16, 'T');
 
 
   // You can configure callback functions that can give feedback to the app user about the current state of the application.
   // In this example we use serial print to demonstrate the call backs. But you could use leds etc.
-  
+
+                                    //  default mode button not connected on LOLIN oled v2.1.0 shield
   IAS.onModeButtonShortPress([]() {
     Serial.println(F(" If mode button is released, I will enter in firmware update mode."));
     Serial.println(F("*-------------------------------------------------------------------------*"));
     dispTemplate_threeLineV2(F("Release"), F("for"), F("Updates"));
   });
-
+                                  //  default mode button not connected on LOLIN oled v2.1.0 shield
   IAS.onModeButtonLongPress([]() {
     Serial.println(F(" If mode button is released, I will enter in configuration mode."));
     Serial.println(F("*-------------------------------------------------------------------------*"));
@@ -116,7 +118,12 @@ void setup() {
   });
 
   IAS.onConfigMode([]() {
-    dispTemplate_threeLineV2(F("Connect to"), F("Wi-Fi"), "xxxxx-" + chipId);
+    //  if wifi clientmode display ip addr
+    if(WiFi.isConnected()){
+      dispTemplate_threeLineV1(F("Connect to"), String(WiFi.SSID()) , WiFi.localIP().toString() );
+      }else{
+      dispTemplate_threeLineV2(F("Connect to"), F("Wi-Fi"), "xxxxx-" + chipId);
+      }
   });
 
   IAS.onFirmwareUpdateCheck([]() {
@@ -125,11 +132,12 @@ void setup() {
 
   IAS.onFirmwareUpdateDownload([]() {
     dispTemplate_threeLineV2(F("Download"), F("&"), F("Install App"));
+    delay(1000);
   });
 
   IAS.onFirmwareUpdateProgress([](int written, int total){
     String perc = String(written / (total / 100)) + "%";
-    dispTemplate_threeLineV2(F("Installing"), perc, F("Done"));
+    dispTemplate_progressBarV1(F("Installing"), perc, written, total );
     Serial.print(".");
   });
 
@@ -144,7 +152,7 @@ void setup() {
 
   IAS.begin();                              // Run IOTAppStory
   IAS.setCallHomeInterval(atoi(updInt));    // Call home interval in seconds(disabled by default), 0 = off, use 60s only for development. Please change it to at least 2 hours in production
-  
+
 
   //-------- Your Setup starts from here ---------------
 
@@ -188,7 +196,7 @@ void loop() {
 
   //-------- Your Sketch starts from here ---------------
 
-  
+  buttonLoop(); // check wemos lolin oled shield v2.1.0 buttons for press event for wemos oled display shield
   if (millis() > tempEntry + 5000 && digitalRead(D3) == HIGH) { // Wait a few seconds between measurements.
     tempEntry = millis();
     // Reading temperature or humidity takes about 250 milliseconds!
@@ -239,12 +247,12 @@ void loop() {
       }else{
         displayTemp(f, F("Fahrenh"));
       }
-      
+
       // Publish to Thingspeak
       if((apikey != NULL) && (apikey[0] != '\0')){
         publishToThingspeak();
       }
-      
+
       // Print to serial
       printLog();
 
@@ -253,7 +261,6 @@ void loop() {
       }
     }
   }
-  
 }
 
 
@@ -274,8 +281,10 @@ void dispTemplate_threeLineV1(String str1, String str2, String str3) {
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.drawString(32, 15, str1);
-  display.drawString(32, 30, str2);
-  display.drawString(32, 45, str3);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(64, 32, str2);
+  display.setTextAlignment(TEXT_ALIGN_RIGHT);
+  display.drawString(96, 51, str3);
   display.display();
 }
 
@@ -360,4 +369,94 @@ void publishToThingspeak() {
   Serial.println(msg);
   delay(1);
   thingspeak.publish(outTopic, msg);
+}
+
+
+void dispTemplate_progressBarV1(String str1, String str2, int written , int total) {
+  int progress = (written / (total / 100));
+  display.clear();
+
+  if(progress < 100) {
+    display.setFont(ArialMT_Plain_10);
+    display.setTextAlignment(TEXT_ALIGN_LEFT);
+    display.drawString(32, 13, str1);
+    display.setFont(ArialMT_Plain_24);
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.drawString(64, 24, str2);
+    display.drawProgressBar(32, 56, 63, 6, progress);
+
+  }else if(progress == 100) {
+    bootScreen(46,14,F("rebooting"),F("/wbmp/logo.WBMP"));
+  }
+  display.display();
+}
+
+   // 0 None
+   // 1 Short Press
+   // 2 Long Press
+   // 3 Double Press
+   // 4 Hold
+   // check wemos lolin oled shield v2.1.0 buttons for press event for wemos oled display shield
+void buttonLoop() {
+  if (button.get() == 0) {
+                                                  //  if button A has been pressed once
+    if (button.BUTTON_A == 1) {
+      dispTemplate_threeLineV1(F("Press twice"), F("for"), F("call home"));
+    }
+    //  if button A has been double pressed
+    if (button.BUTTON_A == 3) {
+      IAS.callHome();                             //  check the IAS server for updates
+    }
+                                                  //  if button B has been pressed once
+    if (button.BUTTON_B == 1) {
+      dispTemplate_threeLineV1(F("Press twice"), F("for"), F("config mode"));
+    }
+                                                  //  if button B has been double pressed
+    if (button.BUTTON_B == 3) {
+      IAS.espRestart('C');                        //  restart in config mode
+    }
+  }
+}
+
+
+void drawWbmp(uint8_t xMove, uint8_t yMove, Stream &file){
+  uint8_t data = file.read();               //  first byte 0 = wbmp
+          data = file.read();               //  second byte 0 = monochrome
+  uint8_t width = file.read();              //  3rd byte = img width max 255 px
+  uint8_t height = file.read();             //  4th byte = img height max 255 px
+    for(uint8_t y = 0; y < height; y++) {
+      for(uint8_t x = 0; x < width; x++ ) {
+        if (x & 7) {
+          data <<= 1; // Move a bit
+        } else {  // Read new data every 8 bit
+          data = file.read();
+        }
+        // if there is a bit draw it
+        if (data & 0x80) {
+          display.setPixel(xMove + x, yMove + y);
+        }
+      }
+    }
+}
+
+void bootScreen(int8_t xMove, int8_t yMove,String str1, const String fileName){
+  if(!SPIFFS.begin()){
+     Serial.println(F("\n\n SPIFFS Mount Failed"));
+     return;
+  }
+  File file = SPIFFS.open(fileName,"r");
+  if (!file) {
+     Serial.println(F("\n\n Failed to open file"));
+     return;
+  }
+
+  display.clear();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.drawString(64, 48, str1);
+  drawWbmp(xMove, yMove, file);
+  display.display();
+
+  file.close();
+  SPIFFS.end();
 }
