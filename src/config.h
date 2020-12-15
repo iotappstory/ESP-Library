a4ac041cc9d503876d9377c930205606941f9708
Onno-Dirkzwager <onno@mentalsolutions.nl>
2020-12-15 23:18:23 +0100

    Update config.h
    Added defines for the configTime function

--- src/config.h
+++ src/config.h
@@ -52,11 +52,16 @@
 // Internal clock
 #if defined  ESP8266
    #define SNTP_INT_CLOCK_UPD           true            // Synchronize the internal clock useing SNTP? BearSSL: This is necessary to verify that the TLS certificates offered by servers are currently valid.
-   #define SNTP_INT_CLOCK_UPD_INTERVAL  43200000        // Clock update internal in miliseconds (default 43200000 = 12 hour)
 #elif defined ESP32
    #define SNTP_INT_CLOCK_UPD           false           // The esp32 uses mbedTLS instead of BearSSL and does not need the time. Your welcome to turn it on for your own projects!
-   #define SNTP_INT_CLOCK_UPD_INTERVAL  43200000        // Clock update internal in miliseconds (default 43200000 = 12 hour)
 #endif
+
+#define SNTP_INT_CLOCK_TIME_ZONE    0                   // Ntp time zone
+#define SNTP_INT_CLOCK_TIME_OFFSET  0                   // Ntp time offset
+#define SNTP_INT_CLOCK_SERV_1       "pool.ntp.org"      // Ntp server 1
+#define SNTP_INT_CLOCK_SERV_2       "time.nist.gov"     // Ntp server 2
+#define SNTP_INT_CLOCK_UPD_INTERVAL 43200000            // Clock update internal in miliseconds (default 43200000 = 12 hour)
+#define SNTP_CONN_MAX_RETRIES       200                 // NTP sync is important! But we cannot wait forever...
 
 // HTTPS defines
 #define HTTPS                       true                // Use HTTPS for OTA updates
