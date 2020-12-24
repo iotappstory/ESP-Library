cdd29b005f89b21892b13d0069138e47d3749330
Onno-Dirkzwager <onno@mentalsolutions.nl>
2020-12-24 23:30:00 +0100

    Update NtpTimeSync.cpp
    Added code to enable the possibillity for users to use the last stored timestamp untill time gets synced with the NTP server. This enables faster reboots into configmode for example. As you do not have to wait for a sync to take place.

--- src/espressif/esp8266/NtpTimeSync.cpp
+++ src/espressif/esp8266/NtpTimeSync.cpp
@@ -28,8 +28,30 @@
 	
 	
 	void NtpTimeSync::ServerSetup(const char* tz, const char* server1, const char* server2, const char* server3) {
+		#if SNTP_USE_STORED_UNTILL_SYNC == true
+			RtcLastNtpSync rtcMem;
+			
+			system_rtc_mem_read(RTCMEMBEGIN, &rtcMem, sizeof(rtcMem));
+			if (rtcMem.markerFlag == MAGICBYTE) {
+				this->Settimeofday(rtcMem.lastKnownTime);
+				this->_timeSet = true;
+			}
+		#endif
+		
+		// settimeofday_cb() is called when settimeofday is called (by SNTP or from your code..)
+		// once enabled (by DHCP), SNTP is updated every hour
 		settimeofday_cb([this]{
 			this->_timeSet = true;
+			
+			#if SNTP_USE_STORED_UNTILL_SYNC == true
+				RtcLastNtpSync rtcMem;
+				
+				time_t now            = time(nullptr);
+				rtcMem.markerFlag     = MAGICBYTE;
+				rtcMem.lastKnownTime  = now;
+				
+				system_rtc_mem_write(RTCMEMBEGIN, &rtcMem, sizeof(rtcMem));
+			#endif
 		});
 	
 		configTime(tz, server1, server2, server3);
