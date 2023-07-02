0c1bde7806c7fae4db154b7a1223d66eddc46165
Christiaan <cwlbroeders@gmail.com>
2023-07-03 01:38:08 +0200

    fixed stuck loop on connection failure and added connection failure switch statement for explanation

--- examples/Other Examples/IAS-PubSub/IAS-PubSub.ino
+++ examples/Other Examples/IAS-PubSub/IAS-PubSub.ino
@@ -1,7 +1,7 @@
 
 /*
-  This sketch is made to show an example of using the popular pubsub mqtt client library
-  made by Nick O'Leary : https://github.com/knolleary/pubsubclient
+  This is an initial sketch to be used as a "blueprint" to create apps which can be used with IOTappstory.com infrastructure
+  Your code can be filled wherever it is marked.
 
   Copyright (c) [2023] [Christiaan Broeders]
 
@@ -41,33 +41,40 @@ WiFiClient wifi;                        //  The client we will be using.
 
 PubSubClient PubSub(wifi);              //  Initialize PubSub and telling it what client is used
 
-//  Declaring some variables, we will use IAS.addField() so we can change them later through our devices config menu 
-char* testMsg = "Hello from the IOTAppStory PubSub demo!";  //  demo text, this is the payload that will be sent
-char* inTopic = "test/iotappstory";                         //  the topic we will subscribe to
-char* outTopic = "test/iotappstory";                        //  the topic we will publish to
-char* broker = "test.mosquitto.org";                        //  the broker server address, in this case the mosquitto test server
-char* LEDpin      = "2";                                    //  we will blink an LED when we get a message
+
+//  Declaring some variables, we will later use IAS.addField() so we can change them later through our devices config menu 
+char* testMsg   = "Hello from the IOTAppstory pubsub example ";   //  demo text, this is the payload that will be sent
+char* inTopic   = "test/iotappstory";                             //  the topic we will subscribe to
+char* outTopic  = "test/iotappstory";                             //  the topic we will publish to
+char* broker    = "192.168.178.2";                                //  the broker server ip address or url
+//  char* broker    = "test.mosquitto.org";
+//  char* broker    = "mqtt.eclipse.org";
+char* LEDpin    = "2";                                            //  we will blink an LED when we get a message
+
+
+
+
 
 unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE  (50)
-char msg[MSG_BUFFER_SIZE];
 
-char* deviceName = "IOTAppstoryPub";
-
+char* deviceName = "IOTAppstory";
+unsigned long now = 0;
 // ================================================ SETUP ================================================
 void setup() {
-  /*
+
   IAS.onFirstBoot([]() {
-    IAS.eraseEEPROM('P');                   // Optional! What to do with EEPROM on First boot of the app? 'F' Fully erase | 'P' Partial erase
+    IAS.eraseEEPROM('P');                   //  Optional! What to do with EEPROM on First boot of the app? 'F' Fully erase | 'P' Partial erase
   });
-  */
-                                            // Adding the variables to our devices config menu so we can change them without having to flash our device
-  IAS.addField(LEDpin, "LedPin", 2, 'P');
-  IAS.addField(testMsg, "Payload", 40);
-  IAS.addField(inTopic, "inTopic", 24);
-  IAS.addField(outTopic, "outTopic", 24);
-  IAS.addField(broker, "Broker", 24);
-  IAS.begin();                              // Run IOTAppStory
+
+ 
+                                            //  Adding the variables to our devices config menu so we can change them without having to flash our device
+  IAS.addField(LEDpin,  "LedPin", 2, 'P');  //  Make a config field so we can assign an LED pin
+  IAS.addField(testMsg, "Payload", 50);     //  a text field to fill in our payload for test purposes
+  IAS.addField(inTopic, "inTopic", 24);     //  a text field to set a topic we want to subscribe to
+  IAS.addField(outTopic,"outTopic", 24);    //  a text field to set the topic we want to publish to
+  IAS.addField(broker,  "Broker", 24);      //  a text field to set the broker address (ip or url)
+  
+  IAS.begin();                              //  Run IOTAppStory
   
 
   //-------- Your Setup starts from here ---------------
@@ -76,6 +83,7 @@ void setup() {
   PubSub.setCallback(callback);             //  set a callback function to run when a we receive a message from a subscribed topic
     
   pinMode(IAS.dPinConv(LEDpin), OUTPUT);
+  reconnect();                              //  Connect to the mqtt broker
 }
 
 
@@ -85,19 +93,12 @@ void loop() {
                 // reaction of the MODEBUTTON pin. If short press (<4 sec): update of sketch, long press (>7 sec): Configuration
                 // reconnecting WiFi when the connection is lost,
                 // and setting the internal clock (ESP8266 for BearSSL)
+  
 
-  
-  //-------- Your Sketch starts from here ---------------
-  
-  
-  
-  if (!PubSub.connected()) {
-    reconnect();
-  }
   PubSub.loop();  //  this routine checks the connection to the mqtt broker
-
-  unsigned long now = millis();
-  if (now - lastMsg > 10000) {
+  //  publish a message to the broker every minute
+  now = millis();
+  if (now - lastMsg > 60000) {
     lastMsg = now;
     Publish(testMsg);
   }
@@ -107,40 +108,33 @@ void loop() {
 
 
 void Publish(const char* mqttPayload){
+  //  check if we are still connected to the broker
     if (PubSub.connected()) {
       Serial.println("Publishing...");
       Serial.println(outTopic);
       Serial.println(mqttPayload);
       PubSub.publish(outTopic, mqttPayload);
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(PubSub.state());
-      delay(1000);
+      reconnect();
     }
   }
 
   void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(IAS.dPinConv(LEDpin), !digitalRead(IAS.dPinConv(LEDpin)));   // Turn the LED on 
-  } else {
-    digitalWrite(IAS.dPinConv(LEDpin), !digitalRead(IAS.dPinConv(LEDpin)));   // Turn the LED off 
-  }
-
+    digitalWrite(2,HIGH);
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    Serial.print("length [");
+    Serial.print(length);
+    Serial.print("]");
+    for (int i = 0; i < length; i++) {
+      Serial.print((char)payload[i]);
+    }
+    Serial.println();
 }
 
 
 void reconnect() {
-  // Loop until we're reconnected
-  while (!PubSub.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
     if (PubSub.connect(deviceName)) {
@@ -148,11 +142,59 @@ void reconnect() {
       // ... and resubscribe
       PubSub.subscribe(inTopic);
     } else {
-      Serial.print("failed, rc=");
-      Serial.print(PubSub.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      Serial.print("failed, rc= ");
+      //  if connection failed, print reason of failure
+      int state = PubSub.state();
+      switch(state){
+        case -4:
+        Serial.print("MQTT_CONNECTION_TIMEOUT");
+        Serial.println("- the server didn't respond within the keepalive time");
+        break;
+        
+        case -3:
+        Serial.print("MQTT_CONNECTION_LOST");
+        Serial.println("- the network connection was broken");
+        break;
+        
+        case -2:
+        Serial.print("MQTT_CONNECT_FAILED");
+        Serial.println("- the network connection failed");
+        break;
+        
+        case -1:
+        Serial.print("MQTT_DISCONNECTED");
+        Serial.println("- the client is disconnected cleanly");
+        break;
+        
+        case 0:
+        Serial.print("MQTT_CONNECTED");
+        Serial.println("- the client is connected");
+        break;
+        
+        case 1:
+        Serial.print("MQTT_CONNECT_BAD_PROTOCOL");
+        Serial.println("- the server doesn't support the requested version of MQTT");
+        break;
+        
+        case 2:
+        Serial.print("MQTT_CONNECT_BAD_CLIENT_ID");
+        Serial.println("- the server rejected the client identifier");
+        break;
+        
+        case 3:
+        Serial.print("MQTT_CONNECT_UNAVAILABLE");
+        Serial.println("- the server was unable to accept the connection");
+        break;
+        
+        case 4:
+        Serial.print("MQTT_CONNECT_BAD_CREDENTIALS");
+        Serial.println("- the username/password were rejected");
+        break;
+        
+        case 5:
+        Serial.print("MQTT_CONNECT_UNAUTHORIZED");
+        Serial.println("- the client was not authorized to connect");
+        break;
+        }
     }
-  }
 }
